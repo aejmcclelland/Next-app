56a48dba19d46eb00587b6ca0f1da86c8e703839
aejmcclelland <aejmcclelland@googlemail.com>
2024-03-25 08:29:55 +0000

    added updateInvoice action and edit form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,13 +14,16 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
+    //Validate the types with Zod
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInPennies = Math.round(amount * 100);
+  const amountInPennies = Math.round(amount * 100); //Converting the amount to pennies
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
@@ -28,7 +31,25 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInPennies}, ${status}, ${date})
   `;
 
+  revalidatePath('/dashboard/invoices'); //clear the client cache and make a new server request
+  redirect('/dashboard/invoices'); //redirect the user to the invoice's page
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInPennies = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInPennies}, status = ${status}
+    WHERE id = ${id}
+  `;
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
